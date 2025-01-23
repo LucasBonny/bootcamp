1d0edc715606b5ac10321139e1d4df2f8a798949
Lucas Bonny <lucas@gunthercloud.com.br>
2025-01-22 23:11:21 -0300

    Find category by id, exception handling

--- backend/src/main/java/br/com/gunthercloud/bootcamp/services/CategoryService.java
+++ backend/src/main/java/br/com/gunthercloud/bootcamp/services/CategoryService.java
@@ -1,6 +1,7 @@
 package br.com.gunthercloud.bootcamp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.gunthercloud.bootcamp.entitites.Category;
 import br.com.gunthercloud.bootcamp.entitites.dto.CategoryDTO;
 import br.com.gunthercloud.bootcamp.repositories.CategoryRepository;
+import br.com.gunthercloud.bootcamp.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,5 +22,11 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
 		return list.stream().map(CategoryDTO::new).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> list = categoryRepository.findById(id);
+		return new CategoryDTO(list.orElseThrow(() -> new EntityNotFoundException("Entity not found!")));
 	}
 }
